7c5dba35c1f6565d0da2299ff031483e695c626c
uanqxzd <105285540+uanqxzd@users.noreply.github.com>
2023-05-06 21:11:54 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,11 +1,11 @@
 #pip install opencv-python
 
-import cv2                              #to import the fetures
+import cv2                              #to import the package
 
-img = cv2.VideoCapture(0)               #deffinge the image
+img = cv2.VideoCapture(0)               #define the image
 while True:
     ret, frame = img.read()             #read the image
-    cv2.imshow("PEGASUS", frame)        #Here it shows
+    cv2.imshow("PEGASUS", frame)        #Here it appears in a window with title
     if cv2.waitKey(1) == ord("q"):      #To end Press q
         print("Quitted..")
 
