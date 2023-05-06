1bbe1f8b4b75ae63e541c4c9e16a7eed7dcd8981
Ervin1562010 <florian.west10@gmail.com>
2023-05-06 18:08:51 +0000

    More Debuging

--- facedetector.py
+++ facedetector.py
@@ -1,29 +1,33 @@
 #pip install opencv-python
 
-import cv2
-
+import cv2                                      #to import funktions
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
 while True:
-    capture = cv2.VideoCapture(0)
+    if GPIO.input(4) == 1:
+        print("gedrückt")
+        capture = cv2.VideoCapture(0)
 
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_from_github.xml")
+        cascade = cv2.CascadeClassifier("haarcascade_frontalface_from_github.xml")
 
-    faces = ()
+        faces = ()
 
-    while faces == ():
-        camera = capture.read()
-        print(camera)
-        im_gray = cv2.cvtColor(camera[1], cv2.COLOR_BGR2GRAY)
-        faces = cascade.detectMultiScale(im_gray)
-        print("faces", faces)                       #Erkennt die Gesichter
+        while faces == ():
+            camera = capture.read()
+            print(camera)
+            im_gray = cv2.cvtColor(camera[1], cv2.COLOR_BGR2GRAY)
+            faces = cascade.detectMultiScale(im_gray)
+            print("faces", faces)                       #Erkennt die Gesichter
 
-        #print(camera[1].empty())
+            #print(camera[1].empty())
 
-    for x, y, width, height in faces:
-        cv2.rectangle(im_gray, (x,y), (x + width, y + height), color=(0,0, 255), thickness=5)
+        for x, y, width, height in faces:
+            cv2.rectangle(im_gray, (x,y), (x + width, y + height), color=(0,0, 255), thickness=5)
 
-    cv2.imshow("PEGASUS", im_gray)
-    cv2.waitKey(10000)
+        cv2.imshow("PEGASUS", im_gray)
+        cv2.waitKey(10000)
 
-    capture.release()
-    continue
-    cv2.destroyAllWindows()
+        capture.release()
+        continue
+        cv2.destroyAllWindows()
